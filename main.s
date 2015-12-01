baa0dc594eb1a1397bb0e6cbd50c55749140de32
ns533 <ns533@njit.edu>
2015-11-30 20:14:54 -0500

    v1.0
    Fixed Reset bug.
    Cleaned up code.
    Set timer to 4-4-2

--- main.s
+++ main.s
@@ -1,22 +1,16 @@
 ;;; Memory Address information found in the KL25P80M48SF0RM document
 SIM_SCGC5   EQU 0x40048038     	;The control for each ports Clock Gate 
-GPIOA_BASE	EQU 0x400FF000 
-GPIOB_BASE	EQU 0x400FF040
-GPIOC_BASE	EQU 0x400FF080
-GPIOD_BASE	EQU 0x400FF0C0
-GPIOE_BASE	EQU 0x400FF100
-;Base address offsets
-GPIO_PDOR	EQU 0x00	;Turn output pin on or off
-GPIO_PSOR	EQU 0x04	;Turn output pin on
-GPIO_PCOR	EQU 0x08	;Turn output pin off
-GPIO_PTOR	EQU 0x0C	;Flip output pin
-GPIO_PDIR	EQU 0x10	;Turn input pin on or off
-GPIO_PDDR	EQU 0x14	;Set as input or output
-;Pins in use
-PORTA_PCR01 EQU 0x40049004  ;LED(red)
-PORTA_PCR02 EQU 0x40049008	;LED(yellow)
-PORTA_PCR04 EQU 0x40049010	;LED(green)
-PORTC_PCR07 EQU 0x4004B01C  ;Button
+SIM_COPC	EQU 0x40048100		;Computer Operating Properly Control
+;Pin Control Registers
+PORTA_PCR01 EQU 0x40049004  	;LED(red)
+PORTA_PCR02 EQU 0x40049008		;LED(yellow)
+PORTA_PCR04 EQU 0x40049010		;LED(green)
+PORTC_PCR07 EQU 0x4004B01C  	;Button
+;Pin Direction & IO Registers
+PORTA_PDDR EQU 0x400FF014
+PORTA_PDOR EQU 0x400FF000
+PORTC_PDDR EQU 0x400FF094
+PORTC_PDIR EQU 0x400FF090	
 	
 ;;; Directives
           PRESERVE8
@@ -44,74 +38,71 @@ Reset_Handler
 ;REMEMBER frometf --m32 option for .srec output!
 
 ;;;;;;;;;;PROGRAM CODE STARTS HERE;;;;;;;;;;;;		
-main
-		LDR R0, =0x00000000
-		LDR R1, =0x00000000
-		LDR R2, =0x00000000
-		LDR R3, =0x00000000
-		LDR R4, =0x00000000
-		LDR R5, =0x00000000
-		LDR R6, =0x00000000
-		LDR R7, =0x00000000
-		
+main	
 		BL initialize			;Initialize I/O ports
 		
-reset	LDR R1, =0x00000002     ;Change light to red
+reset	LDR R6,=0X0				;reset button
+    	LDR R1,=0x00000002      ;Change light to red
 		BL changelight			;
 		
-		LDR R3, =0x00000011		;Put value into counter (1 loop just to show code works)
+		LDR R3,=0x05B8D800		;Put value into counter (4 seconds)
 d30_1 	BL buttonpress			;Check for button press
-		SUBS R3, #17 			;Subtract # of ticks in loop (17) from counter
-		CMP R3, #0
+		SUBS R3,#17 			;Subtract # of ticks in loop (17) from counter
+		CMP R3,#0
 		BGT d30_1
 		
-		CMP R6, #1				;Check for button press
+		CMP R6,#1				;Check for button press
 		BEQ reset				;Reset to red if pressed
-		LDR R1, =0x00000010		;Change light to green
+		LDR R1,=0x00000010		;Change light to green
 		BL changelight			;
 		
-		LDR R3, =0x00D76AB5		;Put value into counter (5 seconds, the board resets when counter is this high)
+		LDR R3,=0x05B8D800		;Put value into counter (4 seconds)
 d30_2	BL buttonpress			;Check for button press
-		SUBS R3, #1 			;Subtract # of ticks in loop (17) from counter
-		CMP R3, #0
+		SUBS R3,#17 			;Subtract # of ticks in loop (17) from counter
+		CMP R3,#0
 		BGT d30_2
 
-		CMP R6, #1				;Check for button press
+		CMP R6,#1				;Check for button press
 		BEQ reset				;Reset to red if pressed
-		LDR R1, =0x00000005		;Change light to yellow
+		LDR R1,=0x00000005		;Change light to yellow
 		BL changelight			;
 		
-		LDR R3, =0x00D76AB5		;Put value into counter (? seconds)
+		LDR R3,=0x02DC6C00		;Put value into counter (2 seconds)
 d10_1	BL buttonpress			;Check for button press
-		SUBS R3, #1 			;Subtract # of ticks in loop (17) from counter
-		CMP R3, #0
+		SUBS R3,#17 			;Subtract # of ticks in loop (17) from counter
+		CMP R3,#0
 		BGT d10_1
         B reset					;Reset to beginning
 
 ;;;Subroutines
 buttonpress 
-		LDR R0, =0x400FF090     ;Put address of PORTC_PDIR into R0
-		LDR R1, [R0]			;Put value of PORTC_PDIR into R1
-		LDR R0, =0x00000080     ;Put value of monitored input pin
-		TST R1, R0				;Check for button press
-		BNE nopress				;Break from process if button not pressed
-		MOVS R6, #1				;Put 1 in R6 if button has been pressed
+		LDR R0,=PORTC_PDIR      ;Put address of PORTC_PDIR into R0
+		LDR R1,[R0]			    ;Put value of PORTC_PDIR into R1
+		LDR R0,=0x00000080      ;Put value of monitored input pin
+		TST R1,R0				;Check for button press
+		BNE nopress				;Break from process if button not pressed (EQ for sim)
+		MOVS R6,#1				;Put 1 in R6 if button has been pressed
 nopress	BX LR					
 
 changelight 
-		LDR R0, =0x400FF000     ;Put address of PORTA_PDOR into R0
-		STR R1, [R0]			;Put value into PDOR
+		LDR R0,=PORTA_PDOR      ;Put address of PORTA_PDOR into R0
+		STR R1,[R0]				;Put value into PDOR
 		BX LR
 			
 initialize
+		;Disable watchdog COP timer
+		LDR R0,=SIM_COPC		;Load address of SIM_COPC to R0
+		LDR R1,=0x0				;Disable watchdog COPT
+		STR R1,[R0]				;
+		
+		
 		;Initialize GPIO clock
-		LDR R0,=SIM_SCGC5     ;Load address of SIM_SCGC5 to R0  
-		LDR R1,[R0]           ;Put value of SIM_SCGC5 into R1  
-		LDR R2,=0x00003F80    ;Load value to turn on all port clocks into R2    
-		ORRS R1,R2            ;OR R2 into R1 for use of masking  
-		STR R1,[R0]           ;Put value back into SIM_SCGC5,   
-							  ;This puts 0011 1110 0000 0000, binary of 3E, into the Sim_SCGC5 register  
-						      ;Which turns on the port clocks A-E.
+		LDR R0,=SIM_SCGC5       ;Load address of SIM_SCGC5 to R0  
+		LDR R1,[R0]             ;Put value of SIM_SCGC5 into R1  
+		LDR R2,=0x00003F80      ;Load value to turn on all port clocks into R2    
+		ORRS R1,R2              ;OR R2 into R1 for use of masking  
+		STR R1,[R0]             ;Put value back into SIM_SCGC5,   
+							  
 		;;Initialize GPIO pins
 		;LED (red)
 		LDR R0,=PORTA_PCR01     ;Load address of PORTA_PCR01 to R0 
@@ -126,18 +117,18 @@ initialize
 		LDR R1,=0x100           ;Set port to GPIO 
 		STR R1,[R0]             ;Put value back into PCR
 		;LED PDDR
-		LDR R0, =0x400FF014      ;Put value of PORTA_PDDR into R0
-		LDR R1, =0x00000016   	 ;Set Pin(s) 1,2,4 to output (0001 0110)
-		STR R1,[R0]              ;Put value back into PDDR
+		LDR R0,=PORTA_PDDR      ;Put value of PORTA_PDDR into R0
+		LDR R1,=0x00000016   	;Set Pin(s) 1,2,4 to output (0001 0110)
+		STR R1,[R0]             ;Put value back into PDDR
 		
 		;Button
 		LDR R0,=PORTC_PCR07     ;Load address of PORTC_PCR07 to R0 
 		LDR R1,=0x103           ;Set port to GPIO and enable pull up. 
 		STR R1,[R0]             ;Put value back into PCR
 		
-		LDR R0, =0x400FF094      ;Put value of PORTC_PDDR into R0
-		LDR R1, =0x00000000   	 ;Set Pins 0-31 to input
-		STR R1,[R0]              ;Put value back into PDDR
+		LDR R0,=PORTC_PDDR      ;Put value of PORTC_PDDR into R0
+		LDR R1,=0x00000000   	;Set Pins 0-31 to input
+		STR R1,[R0]             ;Put value back into PDDR
 		
 		BX LR
 

81640a4c9be421f52e0c2a408f8624e0658130c9
ns533 <ns533@njit.edu>
2015-11-30 22:42:05 -0500

    v1.01 merged buttonpress and delay into a singular subroutine.

--- main.s
+++ main.s
@@ -7,10 +7,10 @@ PORTA_PCR02 EQU 0x40049008		;LED(yellow)
 PORTA_PCR04 EQU 0x40049010		;LED(green)
 PORTC_PCR07 EQU 0x4004B01C  	;Button
 ;Pin Direction & IO Registers
-PORTA_PDDR EQU 0x400FF014
-PORTA_PDOR EQU 0x400FF000
-PORTC_PDDR EQU 0x400FF094
-PORTC_PDIR EQU 0x400FF090	
+PORTA_PDDR  EQU 0x400FF014
+PORTA_PDOR  EQU 0x400FF000
+PORTC_PDDR  EQU 0x400FF094
+PORTC_PDIR  EQU 0x400FF090	
 	
 ;;; Directives
           PRESERVE8
@@ -39,62 +39,53 @@ Reset_Handler
 
 ;;;;;;;;;;PROGRAM CODE STARTS HERE;;;;;;;;;;;;		
 main	
-		BL initialize			;Initialize I/O ports
+		BL initialize			;Initialize board.
 		
-reset	LDR R6,=0X0				;reset button
+reset	LDR R6,=0X0				;Reset button
     	LDR R1,=0x00000002      ;Change light to red
 		BL changelight			;
-		
 		LDR R3,=0x05B8D800		;Put value into counter (4 seconds)
-d30_1 	BL buttonpress			;Check for button press
-		SUBS R3,#17 			;Subtract # of ticks in loop (17) from counter
-		CMP R3,#0
-		BGT d30_1
+		BL delay				;Start delay loop
 		
-		CMP R6,#1				;Check for button press
-		BEQ reset				;Reset to red if pressed
 		LDR R1,=0x00000010		;Change light to green
 		BL changelight			;
-		
 		LDR R3,=0x05B8D800		;Put value into counter (4 seconds)
-d30_2	BL buttonpress			;Check for button press
-		SUBS R3,#17 			;Subtract # of ticks in loop (17) from counter
-		CMP R3,#0
-		BGT d30_2
+		BL delay				;Start delay loop
 
-		CMP R6,#1				;Check for button press
-		BEQ reset				;Reset to red if pressed
 		LDR R1,=0x00000005		;Change light to yellow
 		BL changelight			;
-		
 		LDR R3,=0x02DC6C00		;Put value into counter (2 seconds)
-d10_1	BL buttonpress			;Check for button press
-		SUBS R3,#17 			;Subtract # of ticks in loop (17) from counter
-		CMP R3,#0
-		BGT d10_1
+		BL delay				;Start delay loop
         B reset					;Reset to beginning
 
 ;;;Subroutines
-buttonpress 
-		LDR R0,=PORTC_PDIR      ;Put address of PORTC_PDIR into R0
+;Delay
+delay	LDR R0,=PORTC_PDIR      ;Put address of PORTC_PDIR into R0
 		LDR R1,[R0]			    ;Put value of PORTC_PDIR into R1
 		LDR R0,=0x00000080      ;Put value of monitored input pin
 		TST R1,R0				;Check for button press
-		BNE nopress				;Break from process if button not pressed (EQ for sim)
+		BNE nopress				;Break over next instruction if button not pressed (EQ for sim)
 		MOVS R6,#1				;Put 1 in R6 if button has been pressed
-nopress	BX LR					
+		
+nopress	SUBS R3,#13 			;Subtract # of ticks in loop (13) from counter
+		CMP R3,#0				;Loop again if R3 isn't zero
+		BGT delay				;
+		CMP R6,#1				;Check for button press
+		BEQ reset				;Reset to red if pressed
+		BX LR
 
+;Change LED light
 changelight 
 		LDR R0,=PORTA_PDOR      ;Put address of PORTA_PDOR into R0
 		STR R1,[R0]				;Put value into PDOR
 		BX LR
-			
+
+;Initialize board
 initialize
 		;Disable watchdog COP timer
 		LDR R0,=SIM_COPC		;Load address of SIM_COPC to R0
 		LDR R1,=0x0				;Disable watchdog COPT
 		STR R1,[R0]				;
-		
 		
 		;Initialize GPIO clock
 		LDR R0,=SIM_SCGC5       ;Load address of SIM_SCGC5 to R0  
@@ -131,6 +122,4 @@ initialize
 		STR R1,[R0]             ;Put value back into PDDR
 		
 		BX LR
-
-			
 		END	;End of program
